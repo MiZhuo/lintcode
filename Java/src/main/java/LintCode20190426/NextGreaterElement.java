9315f85894087453589473baf7e82a993f3cf93f
wulibin <wulibin0809@gmail.com>
2019-04-27 09:44:42 +0800

    To be continued

--- Java/src/main/java/LintCode20190426/NextGreaterElement.java
+++ Java/src/main/java/LintCode20190426/NextGreaterElement.java
@@ -6,15 +6,41 @@ public class NextGreaterElement {
     public int nextGreaterElement(int n) {
         // Write your code here
         char[] temp = String.valueOf(n).toCharArray();
-        String s = "";
-        Arrays.sort(temp);
-        for(int i = temp.length -1;i > -1;i--){
-            s += temp[i];
+        char s;
+        int t1 = -1,t2 = -1;
+        for(int j = temp.length - 1; j > -1;j--) {
+            for (int i = j - 1; i > -1; i--) {
+                s = temp[j];
+                if (temp[i] < s) {
+                    t1 = i;
+                    t2 = j;
+                }
+            }
         }
-        int ii = Integer.parseInt(s);
-        if(ii > n){
-            return ii;
+        if(t1 != -1 && t2 != -1) {
+            return Integer.valueOf(swapAndSort(temp, t1, t2));
+        }else {
+            return -1;
         }
-        return -1;
+    }
+
+    private String swapAndSort(char[] chars,int i, int j) {
+        chars[i] = (char) (chars[i] ^ chars[j]);
+        chars[j] = (char) (chars[i] ^ chars[j]);
+        chars[i] = (char) (chars[i] ^ chars[j]);
+        String str1 = "";
+        String str2 = "";
+        for(int k = 0; k < i + 1;k ++){
+            str1 += chars[k];
+        }
+        for(int k = i + 1; k < chars.length;k ++){
+            str2 += chars[k];
+        }
+        char[] chars1 = str2.toCharArray();
+        Arrays.sort(chars1);
+        for (char c : chars1){
+            str1 += c;
+        }
+        return str1;
     }
 }
